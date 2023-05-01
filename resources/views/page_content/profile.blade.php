2b75b54f7db19bcde7e1b1324ea734e26bc914a2
Vincent james <Vincent James Beato>
2023-05-01 19:03:45 +0800

    push n pull

--- resources/views/page_content/profile.blade.php
+++ resources/views/page_content/profile.blade.php
@@ -5,8 +5,13 @@
 
     <div class="container mx-auto m-5" style="max-width: 60%; margin-top: 50px;" alt="User Profile Image">
         <div class="card border-dark p-2">
-            <div class="card-header">
-                <h3>PROFILE</h3>
+            <div class="card-header bg-light">
+                <div class="d-flex justify-content-between align-items-center">
+                    <h3 class="h3 h3-responsive-sm h3-responsive-md h3-responsive-lg" style="text-align: center">USER's TABLE</h3>
+                    <div class="col d-flex justify-content-end">
+                        <a type="button" class="btn btn-light my-3" ><i class="bi bi-three-dots"></i></a>
+                    </div>
+                </div>
             </div>
 
             <div class="card-body">
