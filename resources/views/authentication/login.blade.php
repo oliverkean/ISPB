9c123a6173de21d7210fa9ebf9b57ed2591fc473
Vincent james <Vincent James Beato>
2023-05-01 13:58:44 +0800

    updated login

--- resources/views/authentication/login.blade.php
+++ resources/views/authentication/login.blade.php
@@ -3,6 +3,10 @@
 @section('content')
 
     <div class="container" style="max-width: 800px; margin-top: 50px;">
+        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+            <strong>Access Denied!</strong> You need to login first to access page!
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
         <div class="card p-5">
             <div class="row justify-content-md-between">
                 <div class="col">
@@ -24,15 +28,17 @@
                             <input type="text" class="form-control input-group-sm input-group-md input-group-lg" name="Username" id="Email" value="{{old('Username')}}" placeholder="Username">
                             <label for="Username"><i class="bi bi-person-fill"></i> Username</label>
                             <span class="text-danger">@error('Username') {{$message}} @enderror</span>
+                            <p class="text-danger">Validation error message here</p>
                         </div>
                         <div class="form-floating mb-3">
                             <input type="password" class="form-control" name="Password" id="Password" value="{{old('Password')}}" placeholder="Password">
                             <label for="Password"><i class="bi bi-lock-fill"></i> Password</label>
                             <span class="text-danger">@error('Password') {{$message}} @enderror</span>
+                            <p class="text-danger">Validation error message here</p>
                         </div>
                         <br>
                         <div class="text-center">
-                            <button type="submit" class="btn btn-success btn-sm btn-md btn-lg w-100 rounded-circle-5" id="submit" onclick="loading()">
+                            <button type="submit" class="btn btn-success btn-sm btn-lg w-100 rounded-circle-5" id="submit" onclick="loading()">
                                 <span class="spinner-border spinner-border-sm me-2 d-none" role="status" aria-hidden="true"></span>
                                 <span id="btn-text">Login</span>
                             </button>
