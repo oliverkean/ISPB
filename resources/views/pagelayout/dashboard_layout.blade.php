815fe40ca704b058551d6b9abcc7fce38c5353c6
Ryle Fuentes <20197306@s.ubaguio.edu>
2023-05-02 10:02:07 +0800

    edited some things

--- resources/views/pagelayout/dashboard_layout.blade.php
+++ resources/views/pagelayout/dashboard_layout.blade.php
@@ -37,10 +37,27 @@
         @yield('content')
     </section>
 
+
+
+    <footer class="mb-0">
+        <div class="container mt-5">
+            <div class="container">
+                ISPB
+                <p>
+                    Offering only what's best for you and your animals
+                </p>
+            </div>
+            
+        </div>
+    </footer>
+
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/js/bootstrap.bundle.min.js" integrity="sha512-s+ig1dc29JlU60fXU5y6am5/i5dNnKKsv/CN8Nk6zQ2GcyDv6JysNZX9zlU6/5U5+DvemCryjz3ZmVLMygbyag==" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+
+
+
 </body>
 </html>
