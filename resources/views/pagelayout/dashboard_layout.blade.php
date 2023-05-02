8014a77bef7ced2b758a4089018c4aa850516245
Ryle Fuentes <20197306@s.ubaguio.edu>
2023-05-02 10:28:27 +0800

    deleted footer for now

--- resources/views/pagelayout/dashboard_layout.blade.php
+++ resources/views/pagelayout/dashboard_layout.blade.php
@@ -39,17 +39,7 @@
 
 
 
-    <footer class="mb-0">
-        <div class="container mt-5">
-            <div class="container">
-                ISPB
-                <p>
-                    Offering only what's best for you and your animals
-                </p>
-            </div>
-            
-        </div>
-    </footer>
+    
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/js/bootstrap.bundle.min.js" integrity="sha512-s+ig1dc29JlU60fXU5y6am5/i5dNnKKsv/CN8Nk6zQ2GcyDv6JysNZX9zlU6/5U5+DvemCryjz3ZmVLMygbyag==" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
