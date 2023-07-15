e01d65d0241c98dadbdff058a305c1aafb7172b1
oliverkean <20215296@s.ubaguio.edu>
2023-07-15 17:41:37 +0800

    design v2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,16 +2,13 @@
 @section('title', 'Welcome')
 @section('css', 'manualcss/welcome.css')
 
-
 @section('content')
-
 
 <div class="container-fluid welcome_cont">
     <div class="container welcome_title_container text-center  ">
         <h1 >Khim Ebram's Poultry Business</h1>
         <h3>Inventory System</h3>
     </div>
-    
 </div>
 
 @endsection
